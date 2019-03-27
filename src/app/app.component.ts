db9388bc4d5c43903a89d925f4ec7929aa83c7fd
Manuel <manuel@prototype.berlin>
2019-03-27 08:21:34 +0100

    Configure translate and handle console

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,9 @@ import { Component } from '@angular/core';
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { TranslateService } from '@ngx-translate/core';
+
+import { environment } from '../environments/environment';
 
 @Component({
   selector: 'app-root',
@@ -13,14 +16,46 @@ export class AppComponent {
     private platform: Platform,
     private splashScreen: SplashScreen,
     private statusBar: StatusBar,
+    private translate: TranslateService,
   ) {
     this.initializeApp();
   }
 
   public initializeApp() {
+    this.handleConsole();
+    
     this.platform.ready().then(() => {
+      this.setTranslateConfig();
       this.statusBar.styleDefault();
       this.splashScreen.hide();
     }).catch();
+  }
+
+  private setTranslateConfig() {
+    let lang = navigator.language.split('-')[0];
+    lang = /(de|en)/gi.test(lang) ? lang : 'de';
+
+    this.translate.setDefaultLang('en');
+    // We don't need language switch for this project yet
+    // this.translate.use(lang);
+    this.translate.use('de');
+  }
+
+  private handleConsole() {
+    let methods: string[] = [];
+
+    switch (environment.name) {
+      case 'production':
+      case 'staging':
+        methods = ['log', 'debug', 'info', 'warn'];
+        break;
+
+      default:
+        break;
+    }
+
+    methods.forEach((method) => {
+      console[method] = function () { };
+    });
   }
 }
