f9de990fc7fccf3a7f16e7c5dc4410653707c598
Manuel <manuel@prototype.berlin>
2019-10-02 18:01:34 +0200

    test

--- bin/npm-audit-notify.sh
+++ bin/npm-audit-notify.sh
@@ -3,7 +3,7 @@
 JOB_URL=$1
 PROJECT_NAME=$2
 
-curl -X POST \
+npm audit | grep -q 'high' && curl -s -X POST \
   https://hooks.slack.com/services/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 \
   -H 'Content-Type: application/json' \
   -H 'cache-control: no-cache' \
@@ -16,7 +16,25 @@ curl -X POST \
     		\"title\": \"Found security issues in $PROJECT_NAME\",
     		\"title_link\": \"$JOB_URL\",
     		\"text\": \"Severity: high\",
-    		\"color\": \"danger\"
+    		\"color\": \"warning\"
     	}
     ]
-  }"
+  }" > /dev/null
+
+npm audit | grep -q 'critical' && curl -s -X POST \
+  https://hooks.slack.com/services/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 \
+  -H 'Content-Type: application/json' \
+  -H 'cache-control: no-cache' \
+  -d "{
+    \"channel\": \"i_npm-audit\",
+    \"username\": \"npm audit bot\",
+    \"icon_emoji\": \":rotating_light:\",
+    \"attachments\": [
+    	{
+    		\"title\": \"Found security issues in $PROJECT_NAME\",
+    		\"title_link\": \"$JOB_URL\",
+    		\"text\": \"Severity: critical\",
+    		\"color\": \"critical\"
+    	}
+    ]
+  }" > /dev/null
