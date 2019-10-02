c7ef5ec435680ca9d5d32d5ea39e44d1dba1005b
Max <max@prototype.berlin>
2019-10-02 13:31:15 +0200

    Add order-css script to husky pre-commit instruction

--- scripts/css-order.js
+++ scripts/css-order.js
@@ -2,7 +2,7 @@ const path  = require('path');
 const readline = require('readline');
 const stylelint  = require('stylelint');
 
-const cliQuestion = 'Do you want to fox it? (y for yes)';
+const cliParameter = process.argv[2];
 const green = '\x1b[32m%s\x1b[0m';
 
 let options = {
@@ -16,19 +16,48 @@ stylelint
   .then((data) =>  {
     if (data.errored === false) { return; }
 
+    if (cliParameter === '-f') {
+      options.fix = true;
+      stylelint.lint(options)
+        .catch();
+
+      return;
+    }
     console.log(data.output);
 
-    let interface = readline.createInterface({
-      input: process.stdin,
-      output: process.stdout
-    });
+    if (cliParameter === '-h') {
+      const red = "\x1b[31m";
+      console.log(red, 'You are not in accordance with the CSS linter rules. If you want to fix them automatically type:');
+      console.log(green, " npm run order-css:fix\n");
+      return process.exit(1);
+    }
 
-    interface.question(`\x1b[33m${cliQuestion}\x1b[0m\n`, (answer) => {
-      if (`${answer}` === 'y') {
-        options.fix = true;
-        stylelint.lint(options);
-        console.log(green, 'fixed');
-      }
-      interface.close()
-    });
+    startDialog();
   });
+
+function startDialog() {
+  const cliQuestion = 'Do you want to fix it? (y for yes)';
+  const interface = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
+
+  interface.question(`\x1b[33m${cliQuestion}\x1b[0m\n`, (answer) => {
+    if (`${answer}` !== 'y') {
+      interface.close();
+
+      return;
+    };
+
+    options.fix = true;
+    stylelint.lint(options)
+      .then(() => {
+        interface.close();
+      })
+      .catch(() => {
+        interface.close();
+      });
+
+    console.log(green, 'fixed');
+  });
+}
