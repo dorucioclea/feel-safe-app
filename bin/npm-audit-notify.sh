75a435f4637baaf20ca9e4b5eaa40208c594c405
Manuel <manuel@prototype.berlin>
2019-10-02 17:59:48 +0200

    Test

--- bin/npm-audit-notify.sh
+++ bin/npm-audit-notify.sh
@@ -1,3 +1,5 @@
+#!/bin/sh
+
 JOB_URL=$1
 PROJECT_NAME=$2
 
@@ -5,16 +7,16 @@ curl -X POST \
   https://hooks.slack.com/services/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 \
   -H 'Content-Type: application/json' \
   -H 'cache-control: no-cache' \
-  -d '{
-    "channel": "i_npm-audit",
-    "username": "npm audit bot",
-    "icon_emoji": ":rotating_light:",
-    "attachments": [
+  -d "{
+    \"channel\": \"i_npm-audit\",
+    \"username\": \"npm audit bot\",
+    \"icon_emoji\": \":rotating_light:\",
+    \"attachments\": [
     	{
-    		"title": "Found security issues in $PROJECT_NAME",
-    		"title_link": "$JOB_URL",
-    		"text": "Severity: high",
-    		"color": "danger"
+    		\"title\": \"Found security issues in $PROJECT_NAME\",
+    		\"title_link\": \"$JOB_URL\",
+    		\"text\": \"Severity: high\",
+    		\"color\": \"danger\"
     	}
     ]
-}'
+  }"
