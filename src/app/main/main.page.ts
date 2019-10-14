f644c5dd32041d401549a258ae6ecacbe07e2c1d
Matthias Böhme <matthias@prototype.berlin>
2019-10-14 17:09:01 +0200

    remove tab router direction workaround

--- src/app/main/main.page.ts
+++ src/app/main/main.page.ts
@@ -1,8 +1,4 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
-import { IonTabs, ModalController, NavController } from '@ionic/angular';
-import { NavigationEnd, NavigationStart, Router, RouterEvent } from '@angular/router';
-import { takeUntil } from 'rxjs/operators';
-import { Subject } from 'rxjs/index';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-main',
@@ -11,34 +7,12 @@ import { Subject } from 'rxjs/index';
 })
 // tslint:disable-next-line
 export class MainPage implements OnInit {
-  @ViewChild('tabs', { static: true }) public tabs: IonTabs;
-
-  public activeTabDisabled = false;
-
-  private ngUnsubscribe: Subject<void> = new Subject<void>();
 
   constructor(
-    private modalController: ModalController,
-    private nav: NavController,
-    private router: Router,
   ) {
+
   }
 
   public ngOnInit() {
-    this.router.events
-      .pipe(takeUntil(this.ngUnsubscribe))
-      .subscribe((e: RouterEvent) => {
-        if (e instanceof NavigationStart || e instanceof NavigationEnd) {
-          const canGoBack = this.tabs.outlet.canGoBack();
-
-          this.activeTabDisabled = !canGoBack;
-          // TODO: https://github.com/ionic-team/ionic/issues/18074
-        }
-      })
-  }
-
-  public ngOnDestroy() {
-    this.ngUnsubscribe.next();
-    this.ngUnsubscribe.complete();
   }
 }
